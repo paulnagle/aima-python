f49ab7d4c71cd6a3c0bf60868e0e7c8eea014634
Paul Nagle <paulnagle@gmail.com>
2025-10-28 19:10:33 +0000

    checkpoint

--- assignment2/A2_COMP9016_Nagle_JohnPaul_R00065426.py
+++ assignment2/A2_COMP9016_Nagle_JohnPaul_R00065426.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 """
 A2_COMP9016_Nagle_JohnPaul_R00065426.py
 
@@ -10,8 +9,6 @@ Assignment:   2
 """
 import sys
 import os
-from tracemalloc import take_snapshot
-
 
 
 # Get the parent directory of the current directory
@@ -22,7 +19,6 @@ if parent_dir not in sys.path:
     sys.path.insert(0, parent_dir)
 
 # Now you can import a module from the parent directory
-
 
 from logic import Expr, FolKB, fol_fc_ask
 from utils import expr
@@ -37,14 +33,17 @@ def print_clauses(kb, message="Clauses in the Knowledge Base:", bool_print=True)
 # 1.1.1 Define the Knowledge Base
 print("1.1.1 Define the Knowledge Base" )
 # Create the First Order Logic Knowledge Base
+print("- Creating KB ✔️")
 assignment2_kb = FolKB()
 
 # Define the constants
+print("- Defining constants ✔️")
 Student = Expr('S')         # S is a Student
 Lecturer = Expr('L')        # L is a Lecturer
 Module = Expr('M')          # M is a Module
 
 # Define the predicates
+print("- Defining predicates ✔️")
 Takes = Expr('T')               # T(s, m) s is currently taking module m
 Passed = Expr('P')              # P(s, m) s has already passed module m
 Teaches = Expr('Teaches')       # Teaches(l, m) l teaches module m
@@ -55,13 +54,15 @@ TaughtBy = Expr('TaughtBy')     # TaughtBy(s, l) s is taught by lecturer l
 Classmate = Expr('Classmate')   # Classmate(s, t) share a module and s =\= t
 NotPassed = Expr('NotPassed')   # NotPassed(s, m) s has not passed m
 IndirectPrereq = Expr('IndirectPrereq')  # IndirectPrereq(x, z) x is an indirect prerequisite for z
+NotEqual = Expr('NotEqual')
 
 # Define the premises
+print("- Defining premises ✔️")
 # 1. Teaching implies “taught by” when a student is enrolled
 assignment2_kb.tell(expr('Takes(s, m) & Teaches(l, m) ==> TaughtBy(s, l)'))
 
 # 2. Two students are classmates if they take the same module and are not the same person:
-assignment2_kb.tell(expr('Student(x) & Student(y) & Takes(x, m) & Takes(y, m) ==> Classmate(x, y)'))
+assignment2_kb.tell(expr('Student(x) & Student(y) & Takes(x, m) & Takes(y, m) & NotEqual(x, y) ==> Classmate(x, y)'))
 
 # 3. A student is eligible for a module if they have passed all of its prerequisites:
 assignment2_kb.tell(expr('Prereq(p, m) & Passed(s, p) ==> Eligible(s, m)'))
@@ -75,26 +76,35 @@ assignment2_kb.tell(expr('Prereq(x, y) & Prereq(y, z) ==> IndirectPrereq(x, z)')
 # 1.1.2 Provide the facts
 print("1.1.2 Provide the facts")
 # Modules
+print("- Modules ✔️")
 assignment2_kb.tell(expr('Module(COMP9016)'))
 assignment2_kb.tell(expr('Module(COMP9062)'))
 assignment2_kb.tell(expr('Module(COMP9061)'))
 assignment2_kb.tell(expr('Module(COMP9058)'))
 # Students
+print("- Students ✔️")
 assignment2_kb.tell(expr('Student(Alice)'))
 assignment2_kb.tell(expr('Student(Bob)'))
 assignment2_kb.tell(expr('Student(Eve)'))
+assignment2_kb.tell(expr('NotEqual(Alice, Bob)'))
+assignment2_kb.tell(expr('NotEqual(Alice, Eve)'))
+assignment2_kb.tell(expr('NotEqual(Bob, Eve)'))
 # Lecturers
+print("- Lecturers ✔️")
 assignment2_kb.tell(expr('Lecturer(DrDan)'))
 assignment2_kb.tell(expr('Lecturer(DrSophie)'))
 assignment2_kb.tell(expr('Lecturer(DrLisa)'))
 # Prerequisites
+print("- Prerequisites ✔️")
 assignment2_kb.tell(expr('Prereq(COMP9016, COMP9062)'))
 assignment2_kb.tell(expr('Prereq(COMP9062, COMP9061)'))
 # Teaching
+print("- Teaches ✔️")
 assignment2_kb.tell(expr('Teaches(DrDan, COMP9016)'))
 assignment2_kb.tell(expr('Teaches(DrSophie, COMP9062)'))
 assignment2_kb.tell(expr('Teaches(DrLisa, COMP9061)'))
 # Student Record
+print("- Student records ✔️")
 assignment2_kb.tell(expr('Passed(Alice, COMP9016)'))
 assignment2_kb.tell(expr('Passed(Alice, COMP9058)'))
 assignment2_kb.tell(expr('Passed(Bob, COMP9016)'))
@@ -110,6 +120,7 @@ assignment2_kb.tell(expr('NotPassed(Bob, COMP9058)'))
 assignment2_kb.tell(expr('NotPassed(Bob, COMP9061)'))
 
 # Current Enrolements
+print("- Enrolements ✔️")
 assignment2_kb.tell(expr('Takes(Alice, COMP9062)'))
 assignment2_kb.tell(expr('Takes(Bob, COMP9061)'))
 assignment2_kb.tell(expr('Takes(Eve, COMP9016)'))
@@ -197,6 +208,8 @@ def run_forward_chaining(kb):
     print(f"\nTotal logical inferences: {len(logical_inferences)}")
     return logical_inferences
 
+print_clauses(assignment2_kb, "BEFORE INFERENECE:")
 # Run the forward chaining
 logical_inferences = run_forward_chaining(assignment2_kb)
 
+print_clauses(assignment2_kb, "AFTER INFERENECE:")
